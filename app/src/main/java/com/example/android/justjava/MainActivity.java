77ee40ce5f1a035d4117518c4e5e88382349e3f5
julia <shevchenko.j@gmail.com>
2017-02-28 15:31:28 -0800

    added editable customer name

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,7 +9,10 @@ package com.example.android.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /*** This app displays an order form to order coffee.*/
@@ -26,17 +29,29 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void submitOrder(View view) {
-        int price = calculatePrice();
-        displayMessage(createOrderSummary(price));
-    }
+        CheckBox whippedCream = (CheckBox) findViewById(R.id.whippedCream);
+        boolean hasWhippedCream = whippedCream.isChecked();
 
+        CheckBox chocolate = (CheckBox) findViewById(R.id.chocolate);
+        boolean addChocolate = chocolate.isChecked();
+
+        EditText customerName = (EditText) findViewById(R.id.customerName);
+        Editable addCustomerName = customerName.getText();
+
+        int price = calculatePrice();
+        String priceMessage = createOrderSummary(price, hasWhippedCream, addChocolate, addCustomerName);
+        displayMessage(priceMessage);
+    }
     /***
      * Create summary of the order
      * @param price of the order
-     * @return text summary
+     * @param addWhippedCream is true or false the user wants whipped cream topping
+     *@param addCustomerName @return text summary
      */
-    public String createOrderSummary(int price){
-        String priceMessage = "Name: Vasia Pupkin";
+    public String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate, Editable addCustomerName){
+        String priceMessage = "Name: " + addCustomerName;
+        priceMessage +="\nAdd whipped cream? " + addWhippedCream;
+        priceMessage +="\nAdd Chocolate? " + addChocolate;
         priceMessage += "\nQuantity " + quantity;
         priceMessage += "\nTotal= $" + price + "\nThank  you!";
         return priceMessage;
